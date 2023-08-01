7c1d15829683605a8ac215b035084aca0fd58ef4
HySpeed <hyspeed_games@yahoo.com>
2023-08-01 14:11:34 -0400

    0.2.0
      Changes:
        - Updated naming
        - 18000 tick duration (5 minutes)

--- item.lua
+++ item.lua
@@ -3,6 +3,6 @@ data:extend
     {
         type = "optimized-particle",
         name = "artillery-shell-particle",
-        life_time = 6000 --current time of the shell casing life, assuming ms?!?!?!
+        life_time = 18000 -- time of the shell casing life, ticks (60 / second)?!?!?! (18000 = 5 minutes)
     }
 }
