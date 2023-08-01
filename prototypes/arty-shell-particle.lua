4c109996965d8363d980f96e6a8f151b4f65c9e0
HySpeed <hyspeed_games@yahoo.com>
2023-08-01 14:17:29 -0400

    0.3.1
      Changes:
        - Changed Setting to just minutes
        - Fixed text

--- prototypes/arty-shell-particle.lua
+++
@@ -1,4 +0,0 @@
-local life_time = settings.startup["artillery-shell-particle-life-time"].value -- reads the value from mod settings
-
-data.raw["optimized-particle"]["artillery-shell-particle"].life_time = life_time
-
